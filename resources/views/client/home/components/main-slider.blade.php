6960998c24feea308739d0f1205374d52e48c054
David Villeda <42588903+DavidITT@users.noreply.github.com>
2023-06-13 12:17:02 -0600

    Add banners to client slider

--- resources/views/client/home/components/main-slider.blade.php
+++ resources/views/client/home/components/main-slider.blade.php
@@ -19,12 +19,12 @@
                 <div class="slider-1-height-3 slider-animated-1">
                     <div class="hero-slider-content-2">
                         <h4 class="animated">¿Tienes una marca?</h4>
-                        <h2 class="animated fw-900">Personaliza tus</h2>
+                        <h2 class="animated fw-900 text-white">Personaliza tus</h2>
                         <h1 class="animated fw-900 text-brand">Cajas</h1>
                         <p class="animated">Valido a partir de 25 piezas</p>
                     </div>
                     <div class="slider-img">
-                        <img src="{{asset('assets/client/imgs/slider/banner-3.png')}}" alt="">
+                        <img src="{{asset('assets/client/imgs/slider/banner-2.png')}}" alt="">
                     </div>
                 </div>
             </div>
@@ -38,7 +38,21 @@
 
                     </div>
                     <div class="slider-img">
-                        <img src="{{asset('assets/client/imgs/slider/banner-2.jpg')}}" alt="">
+                        <img src="{{asset('assets/client/imgs/slider/banner-3.png')}}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="single-hero-slider single-animation-wrap">
+            <div class="container">
+                <div class="slider-1-height-3 slider-animated-1">
+                    <div class="hero-slider-content-2">
+                        <h4 class="animated">Tenemos lo que necesitas</h4>
+                        <h2 class="animated fw-900 text-brand">En un lugar</h2>
+
+                    </div>
+                    <div class="slider-img">
+                        <img src="{{asset('assets/client/imgs/slider/banner-4.png')}}" alt="">
                     </div>
                 </div>
             </div>
